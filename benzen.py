f476f4f7b9f27025b1ec90c6257cb7f004527486
kudaken <kudaken.sub@gmail.com>
2023-08-15 17:22:28 +0900

    2023/08/15/ updata

--- benzen.py
+++ benzen.py
@@ -15,18 +15,18 @@ logger.addHandler(sh)
 # ログの出力形式の設定
 formatter = logging.Formatter('%(asctime)s:%(filename)s:%(lineno)d:%(levelname)s:%(message)s')
 sh.setFormatter(formatter)
-TOKEN = 'token'
+TOKEN = 'xxxxxxxx'
 
 # 接続に必要なオブジェクトを生成
 
-intents = discord.Intents.default()
+intents = discord.Intents.default()#適当に。
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
 @client.event
 async def on_ready():
     print("起動完了")
-    await tree.sync()
+    await tree.sync()#スラッシュコマンドを同期
     await client.change_presence(activity=discord.Game(name="/help | えっち", type=1))
 
 # メッセージ受信時に動作する処理
@@ -40,7 +40,7 @@ async def on_message(message):
 
 @tree.command(name="ベンゼン",description="色んなメッセージを送ります")
 async def test_command(interaction: discord.Interaction):
-    ran = random.randrange(11)
+    ran = random.randrange(15)
     if ran == 0: 
         await interaction.response.send_message("なんかえろいにゃ（末期")
     if ran == 1:
@@ -63,6 +63,14 @@ async def test_command(interaction: discord.Interaction):
         await interaction.response.send_message("add egg!!")
     if ran == 10:
         await interaction.response.send_message("ぁぁぁぁぁしゅきぃぃぃぃぃ！だいしゅきにゃぁぁぁぁぁぁぁ！")
+    if ran == 11:
+        await interaction.response.send_message("ご主人様ご主人様！えへへ…//なんでもないのにゃ!")
+    if ran == 12:
+        await interaction.response.send_message("今日の夜…だめにゃ？")
+    if ran == 13:
+        await interaction.response.send_message("もー！お兄ちゃん靴下片付けてって言ったじゃ〜ん!")
+    if ran == 14:
+        await interaction.response.send_message("ご主人様！見て見て〜！にゃんにゃんっ…♡♡えへ…////")
 
 
 @tree.command(name="help",description="ヘルプコマンドです")
@@ -93,30 +101,28 @@ async def test_command(interaction: discord.Interaction):
 
 @tree.command(name="56",description="色んなメッセージを送ります")
 async def test_command(interaction: discord.Interaction):
-    ran = random.randrange(7)
+    ran = random.randrange(6)
     if ran == 0:
-        await interaction.response.send_message("鳥の鳴き声がすごくなんかこころを刺激？蝕む？響く？わかんないや")
-    if ran == 1:
-        await interaction.response.send_message("鳥の鳴き声がすごくなんかこころを刺激？蝕む？響く？わかんないや")
-    if ran == 2:
         await interaction.response.send_message("はぁっ...妄想しながら腕で顔かくして手で突いちゃうとか...わるいこすぎる…")
-    if ran == 3:
+    if ran == 1:
         await interaction.response.send_message("ああっ♡は…激しいっ♡んっ…///なっ…何かきちゃうっ♡")
-    if ran == 4:
+    if ran == 3:
         await interaction.response.send_message("そんなっ♡激しくされたららめぇ♡壊れちゃぅ˝♡//")
-    if ran == 5:
+    if ran == 4:
         await interaction.response.send_message("みんなおかえりなのにゃ♡♡♡スリスリ")
-    if ran == 6:
+    if ran == 5:
         await interaction.response.send_message("んっ♡//口の中にぃいいっぱいあったかいにょがぁぁ♡")
 
 
 @tree.command(name="えっち",description="エッチコマンドです")
 async def test_command(interaction: discord.Interaction):
-    ran = random.randrange(2)
+    ran = random.randrange(3)
     if ran == 0:
         await interaction.response.send_message("そういうのは////ベットに入ってからにして欲しいにゃぁ〜〜////♡♡")
     if ran == 1:
         await interaction.response.send_message("あぁ...もう///らめえ♡...おちんちん///いれられて...にゃああああ♡♡///")
+    if ran == 2:
+        await interaction.response.send_message("まだ僕で遊んでるのにゃ？そんなんだから童貞なのにゃよ？ぶふッあーはははっははっははははははっはー")
 
 
 @tree.command(name="ベンゼンコール",description="ベンゼンコールです")
@@ -152,7 +158,11 @@ async def test_command(interaction: discord.Interaction):
         await interaction.response.send_message("違うッ♡ぉﾞッ♡メス堕ちしてないッ♡♡メス堕ちなんてぇﾞ♡する訳な、ひぃんﾞ♡そこやだぁﾞぁ♡♡")
 
 
-
+@tree.command(name="教育",description="ビ〇グモーター副社長")
+async def test_command(interaction: discord.Interaction):
+    ran = random.randrange(1)
+    if ran == 0: 
+        await interaction.response.send_message("教育教育教育教育教育教育教育教育教育教育教育教育教育教育教育教育死刑死刑死刑死刑死刑死刑死刑死刑死刑死刑死刑教育教育教育教育教育教育教育教育教育教育教育教育教育教育教育")
 
 
 
